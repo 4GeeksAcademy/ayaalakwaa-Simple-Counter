61789dcf854c45fe0ea092a53801dcff7657815f
ayaalakwaa <153412822+ayaalakwaa@users.noreply.github.com>
2024-03-06 21:20:11 +0000

    timer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Card = () => {
+  const [digits, setDigits] = useState([[0], [0], [0], [0], [0], [0]]);
+  const [activeIndex, setActiveIndex] = useState(digits.length - 1);
+  const [isFinished, setIsFinished] = useState(false);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (isFinished) return;
+
+      setDigits((prevDigits) => {
+        const updatedDigits = [...prevDigits];
+
+        let currentValue = updatedDigits[activeIndex][0];
+        currentValue++;
+
+        if (currentValue === 10) {
+          currentValue = 0;
+          if (activeIndex > 0) {
+            setActiveIndex(activeIndex - 1);
+          } else {
+            setIsFinished(true);
+          }
+        }
+
+        updatedDigits[activeIndex][0] = currentValue;
+        return updatedDigits;
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [digits, activeIndex, isFinished]);
+
+  return (
+    <div className="flexCenter text-white gap-3">
+      <div className="flexCenter bg-slategray-600 p-4 rounded border-start fs-1 fw-bold">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="48"
+          height="48"
+          fill="currentColor"
+          class="bi bi-clock"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
+          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
+        </svg>
+      </div>
+      {digits.map((digitArray, index) => (
+        <div
+          key={index}
+          className={`flexCenter bg-slategray-600 p-4 rounded border-start border-end fs-1 fw-bold ${
+            index === activeIndex ? "active" : ""
+          }`}
+        >
+          {digitArray[0]}
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default Home;
+export default Card;
--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,7 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
-
 //import your own components
-import Home from "./component/home.jsx";
-
+import Card from "./component/home.jsx";
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Card />, document.querySelector("#app"));
